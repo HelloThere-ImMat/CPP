821832a7786b2367818c85aeaba636571982c261
Mathias DORR <mdorr@paul-f5Ar5s1.clusters.42paris.fr>
2023-08-14 15:09:43 +0200

    finished the ex04

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -12,17 +12,41 @@
 
 #include <newSed.hpp>
 
-//std::string	replace_in_line(std::string line, t_data data)
-//{
-//	std::string	new_string;
-//	int			i;
+bool	is_replace_word(const char *line_char, int *i, t_data data, std::ofstream &writeFlux)
+{
+	std::string word;
 
-//	i = 0;
-//	while (line[i] != ' ')
-//	{
+	while (line_char[*i] && line_char[*i] != ' ' && line_char[*i] != '\n')
+	{
+		word.push_back(line_char[*i]);
+		(*i)++;
+	}
+	if (word.compare(data.s1) != 0)
+	{
+		writeFlux << word;
+		return (false);
+	}
+	return (true);
+}
 
-//	}
-//}
+void	replace_in_line(std::string line, t_data data, std::ofstream &writeFlux)
+{
+	const char		*line_char;
+	int				i;
+
+	line_char = line.c_str();
+	i = 0;
+	while (line_char[i])
+	{
+		if (is_replace_word(line_char, &i, data, writeFlux) == true)
+			writeFlux << data.s2;
+		if (line_char[i] != '\0')
+		{
+			writeFlux << " ";
+			i++;
+		}
+	}
+}
 
 void	copy_everything(t_data data)
 {
@@ -30,15 +54,11 @@ void	copy_everything(t_data data)
 	std::ofstream	writeFlux(data.replaceFileName, std::ios::out);
 	std::string		line;
 
-	//std::getline(readFlux, line);
-	readFlux >> line;
-	writeFlux << line;
-	while (line.empty() == false)
+	while (readFlux.eof() == false)
 	{
-		std::cout << line << std::endl;
-		readFlux >> line;
-		//replace_in_line(line, data);
-		writeFlux << line;
+		std::getline(readFlux, line);
+		replace_in_line(line, data, writeFlux);
+		writeFlux << std::endl;
 	}
 }
 
