9dfae3dc6218c41533f97afca55427f3a970163d
Mathias DORR <mdorr@made-f0Br6s15.clusters.42paris.fr>
2023-10-17 15:56:54 +0200

    started to fix mod08

--- Module_08/ex00/easyfind.hpp
+++ Module_08/ex00/easyfind.hpp
@@ -6,26 +6,39 @@
 /*   By: mdorr <mdorr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:41:31 by mdorr             #+#    #+#             */
-/*   Updated: 2023/10/05 13:51:50 by mdorr            ###   ########.fr       */
+/*   Updated: 2023/10/17 15:55:25 by mdorr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+
+//template <typename T>
+//int easyfind (T cont, int compare)
+//{
+//	int i = 0;
+//	int lenght = cont.size();
+
+//	while (i < lenght)
+//	{
+//		if (cont[i] == compare)
+//			break ;
+//		i++;
+//	}
+//	if (i == lenght)
+//		return (EXIT_FAILURE);
+//	return (EXIT_SUCCESS);
+//}
 
 template <typename T>
 int easyfind (T cont, int compare)
 {
-	int i = 0;
-	int lenght = cont.size();
+	T::iterator	it = find(cont.begin(), cont.end(), compare);
 
-	while (i < lenght)
-	{
-		if (cont[i] == compare)
-			break ;
-		i++;
-	}
-	if (i == lenght)
+	if (it = cont.end())
 		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
+	else
+		return (EXIT_SUCCESS);
 }
