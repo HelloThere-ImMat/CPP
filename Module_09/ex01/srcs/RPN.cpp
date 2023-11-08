2b17984ad6f57446aa037d48758f9718aa8d12b5
Mathias DORR <mdorr@made-f0Br4s12.clusters.42paris.fr>
2023-11-08 10:41:30 +0100

    bonjour fixed mod09

--- Module_09/ex01/srcs/RPN.cpp
+++ Module_09/ex01/srcs/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: mdorr <mdorr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 12:31:32 by mdorr             #+#    #+#             */
-/*   Updated: 2023/10/28 14:12:42 by mdorr            ###   ########.fr       */
+/*   Updated: 2023/11/08 10:39:39 by mdorr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,76 +112,3 @@ void RPN::printStack()
 		a.pop();
 	}
 }
-
-//int RPN::fillstacks(char *str)
-//{
-//	std::string	instructions = str;
-//	std::string	token;
-//	std::istringstream	iss(instructions);
-//	testStr(instructions);
-//	int	nb;
-
-//	while (iss >> token)
-//	{
-//		if (isOp(token) == true)
-//			_operators.push(whichOperator(token));
-//		else
-//		{
-//			nb = atoi(token.c_str());
-//			if (nb > 10)
-//				throw ("Numbers must always be less than 10");
-//			_operands.push(nb);
-//		}
-//	}
-//	return (EXIT_SUCCESS);
-//}
-
-//void RPN::reverseStacks()
-//{
-//	std::stack<int>	cpy(_operands);
-//	std::stack<e_op> cpy2(_operators);
-//	std::stack<int> tmp;
-//	std::stack<e_op> tmp2;
-
-//	while (cpy.empty() == false)
-//	{
-//		tmp.push(cpy.top());
-//		cpy.pop();
-//	}
-//	_operands = tmp;
-//	while (cpy2.empty() == false)
-//	{
-//		tmp2.push(cpy2.top());
-//		cpy2.pop();
-//	}
-//	_operators = tmp2;
-//}
-
-//void RPN::displaySolution()
-//{
-//	int (*operations[])(int nb1, int nb2) =
-//	{
-//		addition,
-//		soustraction,
-//		multiplication,
-//		division,
-//		error
-//	};
-//	int	nb1;
-//	int nb2;
-//	int res;
-
-//	while (_operators.empty() != true)
-//	{
-//		if (_operands.size() < 2)
-//			throw ("Too many operators");
-//		nb1 = _operands.top();
-//		_operands.pop();
-//		nb2 = _operands.top();
-//		_operands.pop();
-//		res = operations[_operators.top()](nb1, nb2);
-//		_operands.push(res);
-//		_operators.pop();
-//	}
-//	std::cout << _operands.top() << std::endl;
-//}
--- Module_09/ex01/srcs/main.cpp
+++ Module_09/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mdorr <mdorr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 12:36:47 by mdorr             #+#    #+#             */
-/*   Updated: 2023/10/25 16:01:18 by mdorr            ###   ########.fr       */
+/*   Updated: 2023/11/08 10:39:50 by mdorr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,6 @@ int main(int argc, char **argv)
 	}
 	try
 	{
-	 	//weirdCalculator.fillstacks(argv[1]);
-		//weirdCalculator.reverseStacks();
-		//weirdCalculator.printStack();
-		//weirdCalculator.displaySolution();
 		weirdCalculator.makeCalculation(argv[1]);
 	}
 	catch(char const *e)
